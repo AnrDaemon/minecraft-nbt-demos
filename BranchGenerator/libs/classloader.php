7515f7cb2c921cc32673c07667c118ea254c5e09
Andrey Repin <anrdaemon@yandex.ru>
2017-12-05 22:12:27 +0000

    * Import classloader. * Tweak headlines.

--- BranchGenerator/libs/classloader.php
+++ BranchGenerator/libs/classloader.php
@@ -4,9 +4,15 @@
 * @version SVN: $Id$
 */
 
-spl_autoload_register(function($className){
-  $file = new SplFileInfo(__DIR__ . strtr(substr("$className.php", 9), '\\', '/'));
-  $path = $file->getRealPath();
+namespace AnrDaemon;
+
+spl_autoload_register(function($className)
+{
+  $nl = strlen(__NAMESPACE__);
+  if(strncasecmp($className, __NAMESPACE__ . '\\', $nl + 1) !== 0)
+    return;
+
+  $path = realpath(__DIR__ . strtr(substr("$className.php", $nl), '\\', '/'));
   if(!empty($path))
   {
     include_once $path;
