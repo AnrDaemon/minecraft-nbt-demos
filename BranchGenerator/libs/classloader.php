3a2bf482a9d65da52a274ae353281b896fe72ca7
Andrey Repin <anrdaemon@yandex.ru>
2018-11-24 19:12:22 +0000

    * Fixed code style in classloader.

--- BranchGenerator/libs/classloader.php
+++ BranchGenerator/libs/classloader.php
@@ -6,24 +6,27 @@
 
 namespace AnrDaemon;
 
-return \call_user_func(function(){
-  $nsl = \strlen(__NAMESPACE__);
-  return \spl_autoload_register(
-    function($className)
-    use($nsl)
-    {
-      if(\strncmp($className, __NAMESPACE__, $nsl) !== 0)
-        return;
-
-      $className = \substr($className, $nsl);
-      if($className[0] !== "\\")
-        return;
-
-      $path = __DIR__ . \strtr("$className.php", '\\', '/');
-      if(\file_exists($path))
+return \call_user_func(
+  function()
+  {
+    $nsl = \strlen(__NAMESPACE__);
+    return \spl_autoload_register(
+      function($className)
+      use($nsl)
       {
-        return include_once $path;
+        if(\strncmp($className, __NAMESPACE__, $nsl) !== 0)
+          return;
+
+        $className = \substr($className, $nsl);
+        if($className[0] !== "\\")
+          return;
+
+        $path = __DIR__ . \strtr("$className.php", '\\', '/');
+        if(\file_exists($path))
+        {
+          return include_once $path;
+        }
       }
-    }
-  );
-});
+    );
+  }
+);
